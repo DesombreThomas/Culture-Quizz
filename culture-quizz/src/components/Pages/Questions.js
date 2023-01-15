5288d07baeb7e06a9f8b476c82a28e455425e300
DesombreThomas <desombrethomas11@gmail.com>
2023-01-15 14:17:23 +0100

    Mise en place du state pour la dynamisation des questions

--- culture-quizz/src/components/Pages/Questions.js
+++ culture-quizz/src/components/Pages/Questions.js
@@ -1,6 +1,9 @@
 import bg from "../../assets/bg.png";
+import React, { useState } from 'react'
+import { Segment, Dimmer, Loader, Advertisement } from 'semantic-ui-react'
 import useAxios from "../../hooks/useAxios";
 import { useSelector } from "react-redux";
+
 
 
 function Questions() {
@@ -12,7 +15,7 @@ function Questions() {
     amount_of_questions,
     score,
    } = useSelector((state) => state);
-   console.log(questions_category, questions_type, questions_difficulty, amount_of_questions);
+
 
    let apiUrl = `/api.php?amount=${amount_of_questions}`
 
@@ -33,17 +36,38 @@ function Questions() {
 
    console.log(apiUrl);
 
-   const { response, loading } = useAxios({ url: apiUrl })
+   const { response, loading, error } = useAxios({ url: apiUrl });
    console.log(response);
+
+   const [questionIndex, setQuestionIndex] = useState([0]);
    
+
+   if(loading) {
+      return (
+          <Segment className='h-screen'>
+          <Dimmer active>
+            <Loader size='massive'>Loading</Loader>
+          </Dimmer>
+        </Segment>  
+      )
+  }
+
+  if(error) {
+      return (
+        <div>
+          <Advertisement unit='leaderboard' test='Oops Something went wrong !' />
+        </div>  
+      )
+  }
+
 
     return (
       <div className='bg h-screen' style={{backgroundImage: `url(${bg}) `}}>
       <div className='flex flex-col items-center justify-center'>
           <h1 className="flex justify-center text-5xl py-20 text-[#F8EE4F]">Culture Quizz</h1>
               <div className="bg-slate-100 bg-opacity-75 shadow-md shadow-[#040c16] p-3 w-[80%]">
-               <h2 className="text-center"> Question 1 </h2>
-                  <p className="text-2xl font-semibold flex justify-center ">This is the question ?</p>
+               <h2 className="text-center"> Question {questionIndex + 1} </h2>
+                  <p className="text-2xl font-semibold flex justify-center ">{response.results[questionIndex].question}</p>
                 <div className="flex justify-center">
                   <button type="button" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Answer 1</button>
                </div>
